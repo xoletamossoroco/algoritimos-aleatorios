32315636e6902634ca317cb212e14fddc9f4511f
xoletamossoroco <161479727+xoletamossoroco@users.noreply.github.com>
2024-03-25 11:14:37 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print("2-fer")
+
+
+def Grande():
+    nome = input("informe seu nome")
+
+    if not nome:
+        nome = ("você")
+    else:
+        pass
+
+    print(f"um para{nome}, um para mim")
+
+
+Grande()
